d810a3e403706b1527dd93328fb1c2348bb98d37
Joseph Urquhart <josephurquhart4@gmail.com>
2025-03-05 14:25:36 +0000

    Updating content on website

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 <main class="main">
   <section class="section" id="about-me">
     <h1>About Me</h1>
+
+    <p>
+      Hi, my name is <span>Joseph Urquhart</span>. I am currently studying HND Website Development at Edinburgh College.
+      After my HND Website Development course I am planning on furthering my academic career by attending Edinburgh Napier University to complete year 4 and 5 of my degree.
+    </p>
+
+    <p>Once I complete my studies I am planning on pursuing my dream of becoming a <span>full stack</span> website developer.</p>
   </section>
   <section class="section" id="skills">
     <h1>Skills</h1>
