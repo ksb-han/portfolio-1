abc6a46a8e7ad0ca2dfa6697c50f4c1e54f0863a
Joseph Urquhart <josephurquhart4@gmail.com>
2025-03-05 14:14:30 +0000

    Updated Navigation

--- partials/header.php
+++ partials/header.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.css" integrity="sha512-kJlvECunwXftkPwyvHbclArO8wszgBGisiLeuDFwNM8ws+wKIw0sv1os3ClWZOcrEB2eRXULYUsm8OVRGJKwGA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
@@ -16,20 +17,28 @@
 
 <body class="content">
     <header class="header">
-        <nav class="nav">
-            <ul class="nav__links">
-                <li class="nav__link">
-                    <a href="#about-me">About Me <span></span></a>
-                </li>
-                <li class="nav__link">
-                    <a href="#skills">Skills <span></span></a>
-                </li>
-                <li class="nav__link">
-                    <a href="#portfolio">Portfolio <span></span></a>
-                </li>
-                <li class="nav__link">
-                    <a href="#contact-info">Contact Information <span></span></a>
-                </li>
-            </ul>
-        </nav>
+
+        <aside class="side-bar">
+            <nav class="nav">
+                <ul class="nav__links">
+                    <li class="nav__link">
+                        <a href="#about-me">About Me <span></span></a>
+                    </li>
+                    <li class="nav__link">
+                        <a href="#skills">Skills <span></span></a>
+                    </li>
+                    <li class="nav__link">
+                        <a href="#portfolio">Portfolio <span></span></a>
+                    </li>
+                    <li class="nav__link">
+                        <a href="#contact-info">Contact Information <span></span></a>
+                    </li>
+                </ul>
+            </nav>
+
+            <footer class="footer">
+                <a href="https://github.com/Jurquhart93" target="_blank"><i class="ri-github-fill"></i></a>
+                <a href="https://www.linkedin.com/in/joseph-urquhart-9796b8252/" target="_blank"><i class="ri-linkedin-box-fill"></i></a>
+            </footer>
+        </aside>
     </header>
